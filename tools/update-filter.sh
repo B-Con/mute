18dc444548e81a89ffd87b6a757542d8195e2cbc
Brad Conte <brad@bradconte.com>
2013-06-01 03:01:14 -0500

    Added sanity test to site's download script for replacing old file.

--- tools/update-filter.sh
+++ tools/update-filter.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-wget https://raw.github.com/B-Con/mute/master/mute.txt -O /var/www/mute.bradconte.com/public_html/mute.txt
+OUT_FILE=/var/www/mute.bradconte.com/public_html/mute.txt
+wget https://raw.github.com/B-Con/mute/master/mute.txt -O ${OUT_FILE}.tmp
+EXIT_CODE=$?
 
+# Sanity test, so we don't overwrite on a failure garbage.
+LINES=`wc -l ${OUT_FILE}.tmp | cut -f1 -d' '`
+if [ $EXIT_CODE -eq 0 -a $LINES -gt 20 ]; then
+	mv ${OUT_FILE}.tmp ${OUT_FILE}
+fi
